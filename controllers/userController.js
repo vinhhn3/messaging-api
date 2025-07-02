c86b3fef22d16ffdba202e69e7be91e26a9f97be
Vinh Hoang <VinhHn7@fe.edu.vn>
2025-07-02 16:22:24 +0700

    fix bug

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,6 @@
 // controllers/userController.js
 const db = require("../models"); // Import the database connection and models
-const { User, Message, MessageRecipient }  db; // Destructure the User model
+const { User, Message, MessageRecipient } = db; // Destructure the User model
 
 /**
  * @desc Create a new user
